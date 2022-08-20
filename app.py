76f03e2d709527b2f545b7befe52123ff1381057
Tim <harvard90873@gmail.com>
2022-08-20 18:51:40 +0200

    complete app

---
+++ app.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+from flask import Flask, render_template, request
+from nltk.corpus import stopwords
+stopWords = set(stopwords.words('english'))
+
+SAVED_MODEL_DIR = "model"
+
+app = Flask(__name__)
+
+LABELS = ["anger", "fear", "joy", "love", "sadness", "surprise"]
+
+
+@tf.function
+def customStandardization(text: tf.Tensor):
+    # to lower case
+    text = tf.strings.lower(text)
+    # expand contraction
+    pairs = [
+        ("won't", "will not"),
+        ("can't", "can not"),
+        ("n't", " not"),
+        ("'re", " are"),
+        ("'s", " is"),
+        ("'d", " would"),
+        ("'ll", " will"),
+        ("'t", " not"),
+        ("'ve", " have"),
+        ("'m", " am"),
+    ]
+    for contracted, replacement in pairs:
+        text = tf.strings.regex_replace(text, contracted, replacement)
+
+    # clean special symbols
+    text = tf.strings.regex_replace(
+        text, r"\d+(?:\.\d*)?(?:[eE][+-]?\d+)?", " ")
+    text = tf.strings.regex_replace(text, r'@([A-Za-z0-9_]+)', " ")
+    text = tf.strings.regex_replace(text, r"[^A-Za-z0-9]+", " ")
+
+    # remove stopwords
+    for i in stopWords:
+        text = tf.strings.regex_replace(
+            text, f"[^A-Za-z0-9_]+{i}[^A-Za-z0-9_]+", " ")
+
+    return text
+
+
+def loadModel():
+    custom_objects = {"customStandardization": customStandardization}
+    with tf.keras.utils.custom_object_scope(custom_objects):
+        loaded = tf.keras.models.load_model(SAVED_MODEL_DIR)
+        return loaded
+
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        data = request.get_json()
+
+        if not data["text"]:
+            return "Bad Request", 400
+
+        model = loadModel()
+        [res] = model.predict([data["text"]])
+
+        buffer = list(res)
+        final = []
+
+        for _ in range(len(LABELS)):
+            curr = max(buffer)
+            currInd = buffer.index(curr)
+            buffer.remove(curr)
+            final.append({
+                "probability": str(curr),
+                "label": LABELS[currInd].capitalize(),
+            })
+
+        return {
+            "distribution": final,
+        }, 200
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
